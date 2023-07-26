5adc3e1b16502fb759f6484874fd3de5d3f76cda
AnzhelikaKlyushnikova <132193770+AnzhelikaKlyushnikova@users.noreply.github.com>
2023-07-26 23:02:52 +0300

    new changes

--- data_city1.js
+++ data_city1.js
@@ -1,62 +1,63 @@
 
-let input = document.getElementById("data_city");
-let button = document.querySelector('button.add');
-
-let div = document.getElementById("Kras");
-let divAddress = document.getElementById("Kras1");
-let div1 = document.getElementById("Pyat");
-let divAddress1 = document.getElementById("Pyat1");
-let div2 = document.getElementById("Volg");
-let divAddress2 = document.getElementById("Volg1");
-let div3 = document.getElementById("Sochi");
-let divAddress3 = document.getElementById("Sochi1");
-let div4 = document.getElementById("Vlad");
-let divAddress4 = document.getElementById("Vlad1");
-
-let divDelete = document.getElementById("Kras2");
-let divDelete1 = document.getElementById("Pyat2");
-let divDelete2 = document.getElementById("Volg2");
-let divDelete3 = document.getElementById("Sochi2");
-let divDelete4 = document.getElementById("Vlad2");
+let addCity = document.querySelector('.name');
+let buttonAdd = document.querySelector('.add');
+let todoCity = document.querySelector('.addCity');
+let cityList = [];
 
 
+if (localStorage.getItem('todo')){
+    cityList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
+
+buttonAdd.addEventListener('click', function(){
+
+    let newCity = {
+        todo: addCity.value
+    };
+
+    cityList.push(newCity);
+    displayMessages();
+    localStorage.setItem('todo', JSON.stringify(cityList));
+});
+
+function displayMessages(){
+    let displayMessage = '';
+cityList.forEach(function(item, i){
+    displayMessage += `
+    <li class="addLi"
+        <span class="item" id= 'item_${i}'>
+        <span class= 'item_${i}'>${item.todo}</span></span><button class='delete'><img class="deleteImg" src="resources/Component 2.jpg" alt="del"></button>
+    </li>
+    `;
+    todoCity.innerHTML = displayMessage;
+    
+   
+});
+};
+
+let buttonDel = document.querySelector('.delete');
+todoCity.addEventListener('contextmenu', function(event){
+    cityList.forEach(function(item, i){
+        if(item.todoCity === event.target.innerHTML) {
+        if(buttonDel.addEventListener('click', function(){}))
+            cityList.splice(i, 1);
+        displayMessages();
+        localStorage.setItem('todo', JSON.stringify(cityList));
+        }
+
+    })
+})
 
 
-button.onclick = function () {
-       if (input.value === 'Краснодар') {
-       div.style.display = "inline-block";
-       divAddress.style.display = "inline-block";
-       divDelete.style.display = "inline-block";
-       input.value = '';
-    }
-        if (input.value === 'Пятигорск') {
-        div1.style.display = "inline-block";
-        divAddress1.style.display = "inline-block";
-        divDelete1.style.display = "inline-block";
-        input.value = '';
-    }
-        if (input.value === 'Волгоград') {
-        div2.style.display = "inline-block";
-        divAddress2.style.display = "inline-block";
-        divDelete2.style.display = "inline-block";
-        input.value = '';
-    }
-        if (input.value === 'Сочи') {
-        div3.style.display = "inline-block";
-        divAddress3.style.display = "inline-block";
-        divDelete3.style.display = "inline-block";
-        input.value = '';
-    }
-        if (input.value === 'Владикавказ') {
-        div4.style.display = "inline-block";
-        divAddress4.style.display = "inline-block";
-        divDelete4.style.display = "inline-block";
-        input.value = '';
 
-    }
+/*
+console.log()
+    cityList.forEach(function(li){
+        let delLi = document.querySelector('li.addLi')
+        delLi.remove;
 
- };  
-   /* 
+
         if (input.value == "") {
         button.disabled = true;
         input.style.border = "1px solid red";
