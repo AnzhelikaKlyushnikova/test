19fcaa290cb89025b0e442192073562b7ca4e49f
AnzhelikaKlyushnikova <132193770+AnzhelikaKlyushnikova@users.noreply.github.com>
2023-08-01 15:57:33 +0300

    new changes

--- data_city1.js
+++ data_city1.js
@@ -3,6 +3,25 @@ let addCity = document.querySelector('.name');
 let buttonAdd = document.querySelector('.add');
 let todoCity = document.querySelector('.addCity');
 let cityList = [];
+var json_city = [
+    {"id":"defad517-2fc8-43a4-ae01-aa0ef0d2f264",
+    "name":"Сочи",
+    "address":"ул. Роз 117, офис 403"},
+    {"id":"0af9c6f3-cc21-444c-94ec-2d86e654867d",
+    "name":"Пятигорск",
+    "address":"ул. Университетская 28"},
+    {"id":"aa858457-43fd-4c42-82a6-8b6cee759fd9",
+    "name":"Владикавказ",
+    "address":"ул. Весенняя 15Г, офис 317"},
+    {"id":"f197c4cb-0d12-49f7-b758-04cb11c6da07",
+    "name":"Волгоград",
+    "address":"ул. Академическая 22, офис 313"},
+    {"id":"a483f910-8300-427e-aae8-7abc8e7b3b74",
+    "name":"Ростов-на-Дону",
+    "address":"пр-т М.Нагибина, 14а, офис 645"},
+    {"id":"9cabdf58-ca4c-4276-b81a-8b9dbb9f33f3",
+    "name":"Краснодар",
+    "address":"ул. Красная 180, офис 112"}]
 
 
 if (localStorage.getItem('todo')){
@@ -21,26 +40,36 @@ buttonAdd.addEventListener('click', function(){
     localStorage.setItem('todo', JSON.stringify(cityList));
 });
 
-function displayMessages(){
-    let displayMessage = '';
-cityList.forEach(function(item, i){
-    displayMessage += `
+
+
+
+    function displayMessages(){
+    
+        let displayMessage = '';
+    cityList.forEach(function(item, i){
+        displayMessage += `
     <li class="addLi"
         <span class="item" id= 'item_${i}'>
-        <span class= 'item_${i}'>${item.todo}</span></span><button class='delete'><img class="deleteImg" src="resources/Component 2.jpg" alt="del"></button>
+        <span class= 'item_${i}'>${item.todo}</span></span>
     </li>
     `;
-    todoCity.innerHTML = displayMessage;
-    
-   
-});
-};
 
-let buttonDel = document.querySelector('.delete');
+    let btn = document.createElement('button');
+    btn.className = 'delete'
+        
+        buttonDel.addEventListener('click', e => {
+            console.log(btn)
+        })
+
+    })
+}
+
+ 
+
+/*
 todoCity.addEventListener('contextmenu', function(event){
     cityList.forEach(function(item, i){
         if(item.todoCity === event.target.innerHTML) {
-        if(buttonDel.addEventListener('click', function(){}))
             cityList.splice(i, 1);
         displayMessages();
         localStorage.setItem('todo', JSON.stringify(cityList));
@@ -48,11 +77,18 @@ todoCity.addEventListener('contextmenu', function(event){
 
     })
 })
-
+*/
 
 
 /*
+
+
+<button class='delete'><img class="deleteImg" src="resources/Component 2.jpg" alt="del"></button>
+
+
 console.log()
+
+
     cityList.forEach(function(li){
         let delLi = document.querySelector('li.addLi')
         delLi.remove;
